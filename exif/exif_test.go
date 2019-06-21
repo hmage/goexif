99c51d7f40f39fabb930d5d33ae4a2b2a359d38c
Eugene Bujak <hmage@hmage.net>
2019-06-21 13:48:09 +0300

    add benchmark

--- exif/exif_test.go
+++ exif/exif_test.go
@@ -88,6 +88,32 @@ func TestDecodeRawEXIF(t *testing.T) {
 	}
 }
 
+func BenchmarkDecode(b *testing.B) {
+	fpath := filepath.Join(*dataDir, "samples")
+	f, err := os.Open(fpath)
+	if err != nil {
+		b.Fatalf("Could not open sample directory '%s': %v", fpath, err)
+	}
+
+	names, err := f.Readdirnames(0)
+	if err != nil {
+		b.Fatalf("Could not read sample directory '%s': %v", fpath, err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		for _, name := range names {
+			b.Logf("testing file %v", name)
+			f, err := os.Open(filepath.Join(fpath, name))
+			if err != nil {
+				b.Fatal(err)
+			}
+			b.StartTimer()
+			Decode(f)
+			b.StopTimer()
+		}
+	}
+}
+
 type walkFunc func(FieldName, *tiff.Tag) error
 
 func (f walkFunc) Walk(name FieldName, tag *tiff.Tag) error {
